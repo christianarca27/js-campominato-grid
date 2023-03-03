086b46d3863d3c84d7ec448d56c31300a63d12b5
christianarca27 <christian.arca.mail@gmail.com>
2023-03-03 16:44:42 +0100

    feat: svolto bonus

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
+const levelEl = document.getElementById("level");
 const buttonPlayEl = document.getElementById("btn-play");
 
 buttonPlayEl.addEventListener("click", function() {
     const playTableEl = document.getElementById("play-table");
     playTableEl.innerHTML = "";
 
-    for(let i = 1; i <= 100; i++) {
+    let numSquares;
+    if(levelEl.value == "1") {
+        numSquares = 100;
+        widthSquare = "calc(100% / 10)";
+        heightSquare = "calc(100% / 10)";
+    }
+    else if (levelEl.value == "2") {
+        numSquares = 81;
+        widthSquare = "calc(100% / 9)";
+        heightSquare = "calc(100% / 9)";
+    }
+    else {
+        numSquares = 49;
+        widthSquare = "calc(100% / 7)";
+        heightSquare = "calc(100% / 7)";
+    }
+
+    for(let i = 1; i <= numSquares; i++) {
 
         let newEl = document.createElement("div");
         newEl.classList.add("square");
         newEl.innerText = i;
+        newEl.style.width = widthSquare;
+        newEl.style.height = heightSquare;
         playTableEl.append(newEl);
 
         newEl.addEventListener("click", function() {
